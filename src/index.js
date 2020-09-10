466e1e39b5db41219e4a5411d2c55380f091f024
shina <rasheedshinaopeyemi@gmail.com>
2020-09-10 21:54:46 +0100

    chore: eslint-rectify

--- src/index.js
+++ src/index.js
@@ -1,43 +1,41 @@
-import express from 'express'
-import 'dotenv/config'
-import morgan from 'morgan'
-import logger from './config'
+import express from 'express';
+import 'dotenv/config';
+import morgan from 'morgan';
+import logger from './config';
 // import v1Router from './routes'
 
-const app = express()
-const port = process.env.PORT || 5000
-global.logger = logger
+const app = express();
+const port = process.env.PORT || 5000;
+global.logger = logger;
 
-app.use(morgan('combined', { stream: logger.stream }))
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.get('/api/v1', (req, res) =>
-  res.status(200).json({ status: 'success', message: 'Welcome to AMPZ' })
-)
+app.use(morgan('combined', { stream: logger.stream }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.get('/api/v1', (req, res) => res.status(200).json({ status: 'success', message: 'Welcome to AMPZ' }));
 // v1Router(app);
 
 app.use((req, res, next) => {
-  const err = new Error('No endpoint found')
-  err.status = 404
-  next(err)
-})
+  const err = new Error('No endpoint found');
+  err.status = 404;
+  next(err);
+});
 
-app.use((err, req, res, next) => {
+app.use((err, req, res) => {
   logger.error(
     `${err.status || 500} - ${err.message} - ${req.originalUrl} - ${
       req.method
     } - ${req.ip}`
-  )
+  );
   res.status(err.status || 500).json({
     status: 'error',
     error: {
       message: err.message || 'Internal Server Error',
     },
-  })
-})
+  });
+});
 
 app.listen(port, () => {
-  logger.info(`Server running at port ${port}`)
-})
+  logger.info(`Server running at port ${port}`);
+});
 
-export default app
+export default app;
