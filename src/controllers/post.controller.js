893a4a967358a378c7dd216613b26e2c5244dd12
jiokeokwuosa <okwuosachijioke@gmail.com>
2020-10-04 18:15:31 +0100

    feature:user can like and unlike post

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -3,7 +3,9 @@ import Application from '../db/models/application.model';
 import Bookmark from '../db/models/bookmark.model';
 import Comment from '../db/models/comment.model';
 import Like from '../db/models/like.model';
+import PostServices from '../services/post.services';
 import logger from '../config';
+import postServices from '../services/post.services';
 
 /**
  *Contains Post Controller
@@ -125,6 +127,30 @@ class PostController {
         status: 'error',
         error: 'Server error',
       });
+    }
+  }
+
+  /**
+   * like/unlike posts.
+   * @param {Request} req - Response object.
+   * @param {Response} res - The payload.
+   * @memberof PostController
+   * @returns {JSON} - A JSON success response.
+   */
+  static async likePost(req, res) {
+    try {
+      const userId = req.data.id;
+      const {postId} = req.params; 
+
+      const result = await postServices.likedByUser(postId,userId,res);
+      if(result){
+         await postServices.unLike(postId,userId,res);        
+      }else{
+         await postServices.like(postId,userId,res);        
+      }      
+      res.status(200).json({ status: 'success', message:'Post like updated successfully' });
+    } catch (err) {
+      res.status(500).json({ status: 'error', error: 'internal server error' });
     }
   }
 }
