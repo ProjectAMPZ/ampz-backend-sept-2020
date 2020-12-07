032191162a1695ebe5e66554d47f0f31800e7f0b
shina <rasheedshinaopeyemi@gmail.com>
2020-12-07 21:50:57 +0100

    added some

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -172,7 +172,7 @@ class AuthController {
       if (!user.length) {
         return res.status(401).json({
           status: '401 Unauthorized',
-          error: 'invalid credential',
+          error: 'Invalid credential',
         });
       }
       const confirmPassword = await Helper.verifyPassword(
@@ -182,7 +182,7 @@ class AuthController {
       if (!confirmPassword) {
         return res.status(401).json({
           status: '401 Unauthorized',
-          error: 'Invalid Credential',
+          error: 'Invalid credential',
         });
       }
 
